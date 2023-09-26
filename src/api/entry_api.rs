4d2a353ce3dc9e1bee1e73c56cc7e19d2d4d120f
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2023-09-26 17:58:07 -0300

    feat: create entry_api module

---
+++ src/api/entry_api.rs
@@ -0,0 +1,32 @@
+use crate::{models::entry_model::Entry, repositories::entry_repo::EntryRepo};
+use actix_web::{get, web::Data, HttpResponse};
+use chrono::{DateTime, Utc};
+
+#[get("/entries/add")]
+pub async fn add_entry(db: Data<EntryRepo>) -> HttpResponse {
+    let now: DateTime<Utc> = Utc::now();
+    let timestamp = now.to_rfc2822();
+
+    println!("{}", &timestamp);
+
+    let data = Entry {
+        id: None,
+        timestamp,
+    };
+
+    let entry_detail = db.create_entry(data).await;
+
+    match entry_detail {
+        Ok(entry) => HttpResponse::Ok().json(entry),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
+#[get("/entries")]
+pub async fn get_entries(db: Data<EntryRepo>) -> HttpResponse {
+    let entries = db.get_entries().await;
+    match entries {
+        Ok(entries) => HttpResponse::Ok().json(entries),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
