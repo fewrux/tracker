f5021ed120ceea88a59ccd944087fcc3a32d7868
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2023-09-27 10:08:39 -0300

    doc: add documentation and implementation comments

--- src/api/entry_api.rs
+++ src/api/entry_api.rs
@@ -2,6 +2,17 @@ use crate::repositories::entry_repo::EntryRepo;
 use actix_web::{get, web::Data, HttpResponse};
 use chrono::{DateTime, Utc};
 
+/// Adds a new entry to the database with the current timestamp.
+///
+/// This endpoint generates a timestamp in RFC2822 format for the new entry and
+/// adds it to the database using the `EntryRepo`. If successful, it returns the
+/// details of the added entry in JSON format.
+///
+/// # Example
+///
+/// ```
+/// GET /entries/add
+/// ```
 #[get("/entries/add")]
 pub async fn add_entry(db: Data<EntryRepo>) -> HttpResponse {
     let now: DateTime<Utc> = Utc::now();
@@ -21,6 +32,16 @@ pub async fn add_entry(db: Data<EntryRepo>) -> HttpResponse {
     }
 }
 
+/// Retrieves a list of all entries in the database.
+///
+/// This endpoint retrieves a list of entries from the database using the `EntryRepo`.
+/// If successful, it returns the list of entries in JSON format.
+///
+/// # Example
+///
+/// ```
+/// GET /entries
+/// ```
 #[get("/entries")]
 pub async fn get_entries(db: Data<EntryRepo>) -> HttpResponse {
     let entries = db.get_entries().await;
--- src/repositories/entry_repo.rs
+++ src/repositories/entry_repo.rs
@@ -4,39 +4,74 @@ use futures::TryStreamExt;
 
 use mongodb::{bson::doc, options::FindOptions, results::InsertOneResult, Client, Collection};
 
+/// Repository for managing `Entry` documents in the database.
 pub struct EntryRepo {
     col: Collection<Entry>,
 }
 
 impl EntryRepo {
+    /// Initializes a new instance of `EntryRepo` connected to the MongoDB database.
+    ///
+    /// # Arguments
+    ///
+    /// * `uri` - A string containing the MongoDB connection URI.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the initialized `EntryRepo` instance if successful, or an error if the connection fails.
     pub async fn init(uri: String) -> Result<Self, anyhow::Error> {
+        // Create a MongoDB client using the provided URI.
         let client = Client::with_uri_str(uri).await.map_err(|e| {
             log::error!("\n{e:?}");
             anyhow!("failed to connect to database: {}", e)
         })?;
+
+        // Get the database and collection handles.
         let db = client.database("tracker_mongo");
         let col = db.collection("Entry");
+
+        // Return the initialized EntryRepo.
         Ok(EntryRepo { col })
     }
 
+    /// Creates a new entry in the database with the given timestamp.
+    ///
+    /// # Arguments
+    ///
+    /// * `timestamp` - A string representing the timestamp for the new entry.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the result of the insertion operation if successful, or an error if it fails.
     pub async fn create_entry(&self, timestamp: String) -> Result<InsertOneResult, anyhow::Error> {
+        // Create a new Entry document.
         let new_doc = Entry {
             id: None,
             timestamp,
         };
 
+        // Insert the new Entry document into the collection
         let entry = self.col.insert_one(new_doc, None).await.map_err(|e| {
             log::error!("\n{e:?}");
             anyhow!("error adding new entry: {}", e)
         })?;
+
+        // Return the result of the insertion.
         Ok(entry)
     }
 
+    /// Retrieves a list of entries from the database, sorted by timestamp in descending order.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing a vector of `Entry` instances if successful, or an error if the retrieval fails.
     pub async fn get_entries(&self) -> Result<Vec<Entry>, anyhow::Error> {
+        // Define options for the find operation to sort by timestamp in descending order.
         let find_options = FindOptions::builder()
             .sort(doc! { "timestamp": -1 })
             .build();
 
+        // Perform the find operation on the database.
         let mut cursors = self.col.find(None, find_options).await.map_err(|e| {
             log::error!("\n{e:?}");
             anyhow!("error getting list of entries: {}", e)
@@ -44,6 +79,7 @@ impl EntryRepo {
 
         let mut entries: Vec<Entry> = Vec::new();
 
+        // Iterate through the cursor and collect entries.
         while let Some(entry) = cursors.try_next().await.map_err(|e| {
             log::error!("\n{e:?}");
             anyhow!("error mapping through cursor: {}", e)
@@ -51,6 +87,7 @@ impl EntryRepo {
             entries.push(entry)
         }
 
+        // Return the list of retrieved entries.
         Ok(entries)
     }
 }
