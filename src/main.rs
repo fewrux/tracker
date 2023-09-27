d6d50ca814cb527f7588c1432d9fc8e9a24ce6d6
Felipe Rubio <felipe.carvalho.rubio@gmail.com>
2023-09-26 22:39:36 -0300

    feat: add anyhow crate for error handling

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@ mod repositories;
 
 use actix_web::web::{Data, ServiceConfig};
 use anyhow::anyhow;
-use api::entry_api::{add_entry, get_entries};
-use repositories::entry_repo::EntryRepo;
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_secrets::SecretStore;
+
+use api::entry_api::{add_entry, get_entries};
+use repositories::entry_repo::EntryRepo;
 
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    let db_uri = if let Some(secret) = secret_store.get("MONGO_URI") {
-        secret
-    } else {
-        return Err(anyhow!("Error loading database uri").into());
+    let db_uri = match secret_store.get("MONGO_URI") {
+        Some(secret) => secret,
+        None => return Err(anyhow!("failed to load database URI").into()),
     };
 
     let db = EntryRepo::init(db_uri).await;
